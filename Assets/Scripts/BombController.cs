a299b0c840025305aae3c1c7d8903f420c51de78
tus-eva-2021 <k00267259@student.lit.ie>
2023-02-21 14:10:11 +0000

    Coroutine Script Not working
    the script is written correctly but its not executing when w is pressed

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -7,23 +7,34 @@ public class BombController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKeyDown("w"))
         {
             StartCoroutine("FadeCoroutine");
         }
-    IEnumerator FadeCoroutine()
-            for (float f = 1; f >= 0; f -= 0.1){
+    
+    }
+private IEnumerator FadeCoroutine() { 
+        for (float f = 1; f >= 0; f -= 0.1f){
         Color c = spriteRenderer.color;
             c.a = f;
             spriteRenderer.color = c;
-            yield return new WaitForSeconds(1f);
+         
+            yield return new WaitForSeconds(5f);
+
+            ResetSpriteRenderer();
         }
+    }
+    void ResetSpriteRenderer()
+    {
+        Color c = spriteRenderer.color;
+        c.a = 1f;
+        spriteRenderer.color = c;
     }
 
 }
